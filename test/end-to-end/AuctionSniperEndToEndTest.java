249938e7fa7a5a0fa8db46900aec2cec9f02c185
DannKim57 <il0214li@daum.net>
2020-07-13 13:19:40 +0900

    ch10 The Walking Skeleton

---
+++ test/end-to-end/AuctionSniperEndToEndTest.java
@@ -0,0 +1,32 @@
+import org.junit.After;
+import org.junit.Test;
+
+public class AuctionSniperEndToEndTest {
+    private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+    private final ApplicationRunner application = new ApplicationRunner();
+
+    @Test public void sniperJoinsAuctionUntilAuctionCloses() {
+        auction.startSellingItem();
+        application.startBiddingIn(auction);
+        auction.hasReceivedJoinRequestFromSniper();
+        auction.announceClosed();
+        application.showsSniperHasLostAuction();
+    }
+
+    // Additional Cleanup
+    @After public void stopAuction() {
+        auction.stop();
+    }
+    @After public void stopApplication() {
+        application.stop();
+    }
+}
+
+// This first test is not really end-to-end. 
+// It doesn't include the real auction service 
+// because that is not easily available. 
+
+// Must-have skill**
+// An important part of the test-driven development skills is
+// judging where to set the boundaries of what to test
+// and how to eventually cover everything.
---
+++ todolist.md
@@ -0,0 +1,7 @@
+Single item - join, lose without bidding
+Single item - join, bid & lose
+Single item - join, bid & wind
+Single item - show price details
+Multiple items
+Add new items through the GUI
+Stop bidding at stop price
