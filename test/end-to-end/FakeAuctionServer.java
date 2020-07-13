7eff2798892d25cd312679fa5ecfe062f38828d1
DannKim57 <il0214li@daum.net>
2020-07-13 16:59:14 +0900

    ch11 Passing the First Test: the Fake Auction 2

--- test/end-to-end/FakeAuctionServer.java
+++ test/end-to-end/FakeAuctionServer.java
@@ -1,11 +1,21 @@
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.ChatManagerListener;
+import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class FakeAuctionServer {
+    private final SingleMessageListener messageListener = new SingleMessageListener();
+    
     public static final String ITEM_ID_AS_LOGIN = "auction-%s";
     public static final String AUCTION_RESOURCE = "Auction";
     public static final String XMPP_HOSTNAME = "localhost";
@@ -28,11 +38,35 @@ public class FakeAuctionServer {
             new ChatManagerListener(){ // Q. Who uses the method chatCreated()?? Does Listener use this?
                 public void chatCreated(Chat chat, boolean createdLocally) {
                     currentChat = chat;
+                    chat.addMessageListener(messageListener);
                 }
             });
     }
 
     public String getItemId() {
         return itemId;
+    }
+
+    public void hasReceivedRequestFromSniper() throws InterruptedException {
+        messageListener.receivesAMessage();
+    }
+
+    public void announceClosed() throws XMPPException {
+        currentChat.sendMessage(new Message());
+    }
+
+    public void stop() {
+        connection.disconnect();
+    }
+
+    public class SingleMessageListener implements MessageListener {
+        private final ArrayBlockingQueue<Message> messages = 
+                                    new ArrayBlockingQueue<>(1);
+        public void processMessage(Chat chat, Message message) {
+            messages.add(message);
+        } 
+        public void receivesAMessage() throws InterruptedException {
+            assertThat("Message", messages.poll(5, TimeUnit.SECONDS), is(notNullValue()));
+        }
     }
 }
