f14eac667988b20ae08c8a69740414236ad1f408
DannKim57 <il0214li@daum.net>
2020-07-16 17:40:48 +0900

    refered to p.116, never invoked: auctionClosed(), but I passed... need to figure out

---
+++ src/auctionsniper/xmpp/AuctionMessageTranslator.java
@@ -0,0 +1,12 @@
+package auctionsniper.xmpp;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.packet.Message;
+
+public class AuctionMessageTranslator {
+
+	public void processMessage(Chat unusedChat, Message message) {
+        // TODO Fill in here
+	}
+
+}
--- test/unit/xmpp/AuctionMessageTranslatorTest.java
+++ test/unit/xmpp/AuctionMessageTranslatorTest.java
@@ -1,11 +1,38 @@
 package xmpp;
 
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.packet.Message;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
 import org.junit.Test;
 
+import auctionsniper.AuctionEventListener;
+import auctionsniper.xmpp.AuctionMessageTranslator;
+
 public class AuctionMessageTranslatorTest {
+    public static final Chat UNUSED_CHAT = null;
+    private final Mockery context = new Mockery();
+    private final AuctionEventListener listener = 
+                    context.mock(AuctionEventListener.class);
+    private final AuctionMessageTranslator translator = 
+                    new AuctionMessageTranslator();
+
     @Test public void
     notifiesAuctionClosedWhenCloseMessageReceived() {
-        // nothing yet
+        context.checking(new Expectations() {
+            {
+                oneOf(listener).auctionClosed();
+            }
+            // @Override
+            // public <T> T oneOf(T mockObject) {
+            //     // TODO Auto-generated method stub
+            //     return super.oneOf(mockObject);
+            // } // {oneOf()} vs oneOf() **
+        });        
+        Message message = new Message();
+        message.setBody("SOLVersion: 1.1; Event: CLOSE;");
+
+        translator.processMessage(UNUSED_CHAT, message);
     }
 }
 
