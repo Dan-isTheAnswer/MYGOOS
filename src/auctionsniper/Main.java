061b784af37bfdd89eacca70253878cb0cc5ad49
DannKim57 <il0214li@daum.net>
2020-07-17 16:43:41 +0900

    ch13 the Sniper Makes a Bid / a New Clas, with Dependencies

--- src/auctionsniper/Main.java
+++ src/auctionsniper/Main.java
@@ -14,7 +14,7 @@ import auctionsniper.xmpp.AuctionMessageTranslator;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class Main implements AuctionEventListener {
+public class Main implements SniperListener {
     @SuppressWarnings("unusesd") private Chat notToBeGCd;
     private static final int ARG_HOSTNAME = 0;
     private static final int ARG_USERNAME = 1;
@@ -55,11 +55,11 @@ public class Main implements AuctionEventListener {
         disconnectWhenUICloses(connection);
         Chat chat = connection.getChatManager().createChat(
             auctionId(itemId, connection), 
-            new AuctionMessageTranslator(this));
-        // this.notToBeGCd = chat;
+            new AuctionMessageTranslator(new AuctionSniper(this)));
+        this.notToBeGCd = chat;
 
         chat.sendMessage(JOIN_COMMAND_FORMAT);
-        notToBeGCd = chat;
+        // notToBeGCd = chat;
     }
 
     private void disconnectWhenUICloses(final XMPPConnection connection) {
@@ -86,19 +86,14 @@ public class Main implements AuctionEventListener {
         });
     }
 
+
     @Override
-    public void auctionClosed() {
-        SwingUtilities.invokeLater(new Runnable(){
+    public void sniperLost() {
+        SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 ui.showStatus(MainWindow.STATUS_LOST);
             }
         });
-    }
-
-    @Override
-    public void currentPrice(int price, int increment) {
-        // TODO Auto-generated method stub
-
     }
 }
