ec61eba78e84c4bae7c823dabdfb487ba99bb66f
Dan <dankim0213@gmail.com>
2020-07-19 19:12:31 +0900

    ch15 updated

--- src/auctionsniper/SniperState.java
+++ src/auctionsniper/SniperState.java
@@ -1,13 +1,9 @@
 package auctionsniper;
 
-public class SniperState {
-    public final String itemId;
-    public final int lastPrice;
-    public final int lastBid;
-
-    public SniperState(String itemId, int lastPrice, int lastBid) {
-        this.itemId = itemId;
-        this.lastPrice = lastPrice;
-        this.lastBid = lastBid;
-    }
+public enum SniperState {
+  JOINING,
+  WINNING,
+  BIDDING,
+  LOST,
+  WON;
 }
--- test/unit/AuctionSniperTest.java
+++ test/unit/AuctionSniperTest.java
@@ -1,3 +1,5 @@
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.Matcher;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.jmock.States;
@@ -8,6 +10,7 @@ import org.junit.runner.RunWith;
 import auctionsniper.Auction;
 import auctionsniper.AuctionSniper;
 import auctionsniper.SniperListener;
+import auctionsniper.SniperSnapshot;
 import auctionsniper.SniperState;
 import auctionsniper.AuctionEventListener.PriceSource;
 
@@ -20,8 +23,8 @@ public class AuctionSniperTest {
     private final AuctionSniper sniper = new AuctionSniper(auction, sniperListener);
     private final States sniperState = context.states("sniper");
 
-    @Test public void 
-    reportLostWhenAuctionClosesImmediately() {
+    @Test
+    public void reportLostWhenAuctionClosesImmediately() {
         context.checking(new Expectations() {
             {
                 atLeast(1).of(sniperListener).sniperLost();
@@ -29,24 +32,40 @@ public class AuctionSniperTest {
         });
 
         sniper.auctionClosed();
-    } 
+    }
 
     @Test public void
     reportsLostIfAuctionClosesWhenBidding() {
         context.checking(new Expectations() {{
             ignoring(auction);
-            allowing(sniperListener).sniperBidding(new SniperState(ITEM_ID, 0, 0) );
-                    then(sniperState.is("Bidding"));
+            allowing(sniperListener).sniperStateChanged(
+                                        with(aSniperThatIs(SniperState.BIDDING));
+                                            then(sniperState.is("bidding"));
+            )
 
-            atLeast(1).of(sniperListener).sniperLost();
-                    when(sniperState.is("bidding"));
+            atLeast(1).of(sniperListener).sniperStateChanged(
+                                            new SniperSnapshot(ITEM_ID, 135, 135, SniperState.WINNING));
+                                                when(sniperState.is("bidding"));
         }});
 
-        sniper.currentPrice(123, 45, PriceSource.FromOtherBidder);
-        sniper.auctionClosed();
+        sniper.currentPrice(123, 12, PriceSource.FromOtherBidder);
+        sniper.currentPrice(135, 45, PriceSource.FromSniper);
+        // sniper.auctionClosed();
     }
 
-    @Test public void
+    protected Matcher<SniperSnapshot> aSniperThatIs(final SniperState bidding) {
+        return new FeatureMatcher<SniperSnapshot,SniperState>(
+            equalTo(state), "sniper that is", "was")
+        {
+            @Override
+            protected SniperState featureValueOf(SniperSnapshot actual) {
+                return actual.state;
+            }
+        };
+    }
+
+    @Test
+    public void
     reportsWonIfAuctionClosesWhenWinning() {
         context.checking(new Expectations() {
             {
