95399cde0114e2493579604eeb9f7db94ee91298
DannKim57 <il0214li@daum.net>
2020-07-21 15:50:26 +0900

    updated: Teasing out a new Class

---
+++ src/auctionsniper/AuctionEventListener.java
@@ -0,0 +1,5 @@
+package unit;
+
+public interface AuctionEventListener {
+    public void auctionClosed();
+}
--- test/unit/unit/AuctionMessageTranslatorTest.java
+++ test/unit/unit/AuctionMessageTranslatorTest.java
@@ -2,20 +2,31 @@ package unit;
 
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.packet.Message;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import auctionsniper.AuctionEventListener;
 import auctionsniper.AuctionMessageTranslator;
 
+@RunWith(JMock.class) 
 public class AuctionMessageTranslatorTest {
     public static final Chat UNUSED_CHAT = null;
+    private final Mockery context = new Mockery();
+    private final AuctionEventListener listener =
+        context.mock(AuctionEventListener.class);
     private final AuctionMessageTranslator translator =
         new AuctionMessageTranslator();
     
     @Test public void
     notfiesAuctionClosedWhenCloseMessageReceived() {
+        context.checking(new Expectations() {{
+            oneOf(listener).auctionClosed();
+        }});
         Message message = new Message();
         message.setBody("SOLVersion: 1.1; Event: CLOSE;");
-        
         translator.processMessage(UNUSED_CHAT, message);
     }
 }
