1f4cf402428fb8eea8bfcd591309cbbac2888178
DannKim57 <il0214li@daum.net>
2020-07-21 18:51:39 +0900

    solved: a Test for Bidding

--- src/auctionsniper/Main.java
+++ src/auctionsniper/Main.java
@@ -56,10 +56,20 @@ public class Main implements SniperListener {
     private void joinAuction(XMPPConnection connection, String itemId)
     throws XMPPException {
         disconnectWhenUICloses(connection);
-        final Chat chat = connection.getChatManager().createChat(
-            auctionId(itemId, connection),
-            new AuctionMessageTranslator(new AuctionSniper(this)));
+        final Chat chat =
+            connection.getChatManager().createChat(auctionId(itemId, connection), null);
         this.notToBeGCd = chat;
+        Auction auction = new Auction() {
+            public void bid(int amount) {
+                try {
+                    chat.sendMessage(String.format(BID_COMMAND_FORMAT, amount));
+                } catch (XMPPException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        chat.addMessageListener(
+        new AuctionMessageTranslator(new AuctionSniper(auction, this)));
         chat.sendMessage(JOIN_COMMAND_FORMAT);
     }
 
@@ -88,6 +98,15 @@ public class Main implements SniperListener {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 ui.showStatus(MainWindow.STATUS_LOST);
+            }
+        });
+    }
+
+    @Override
+    public void sniperBidding() {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                ui.showStatus(MainWindow.STATUS_BIDDING);
             }
         });
     }
