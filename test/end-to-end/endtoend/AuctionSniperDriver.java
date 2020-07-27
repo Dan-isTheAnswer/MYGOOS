1fb969825a0c726f016be530ddf7b6f4819ac715
Dan <dankim0213@gmail.com>
2020-07-27 20:52:27 +0900

    -failed: Sending the State out of the Sniper

--- test/end-to-end/endtoend/AuctionSniperDriver.java
+++ test/end-to-end/endtoend/AuctionSniperDriver.java
@@ -2,14 +2,13 @@ package endtoend;
 
 import com.objogate.wl.swing.AWTEventQueueProber;
 import com.objogate.wl.swing.driver.JFrameDriver;
-import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.driver.JTableDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 
 import auctionsniper.Main;
 
-import static org.hamcrest.Matchers.equalTo;
-// import static auctionsniper.Main.MAIN_WINDOW_NAME;
-// import static auctionsniper.Main.SNIPER_STATUS_NAME;
+import static com.objogate.wl.swing.matcher.IterableComponentsMatcher.matching;
+import static com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText;
 
 public class AuctionSniperDriver extends JFrameDriver {
     public AuctionSniperDriver(int timeoutMillis) {
@@ -21,8 +20,12 @@ public class AuctionSniperDriver extends JFrameDriver {
         );
     } // JFrameDriver constructor ??
 
-    public void showsSniperStatus(String statusText) {
-        new JLabelDriver(
-            this, named(Main.SNIPER_STATUS_NAME)).hasText(equalTo(statusText));
+    public void showsSniperStatus(String itemId, int lastPrice, int lastBid,
+                                    String statusText) 
+    {
+        JTableDriver table = new JTableDriver(this);
+        table.hasRow(
+            matching(withLabelText(itemId), withLabelText(String.valueOf(lastPrice)),
+                withLabelText(String.valueOf(lastBid)), withLabelText(statusText)));;
     }
 }
